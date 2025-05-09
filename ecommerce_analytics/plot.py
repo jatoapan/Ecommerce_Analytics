93d22072893a9ba8bf0e32dc59254ba7704509dd
jatoapan <jatoapan@espol.edu.ec>
2025-05-08 22:58:31 -0500

    chore: starting data preparation analysis

--- ecommerce_analytics/plot.py
+++ ecommerce_analytics/plot.py
@@ -4,14 +4,15 @@ import seaborn as sns
 
 def plot_completeness_barplot(df, column, df_name=None):
     completeness_df = df[column]
-    completeness_df = completeness_df.isna().agg(["sum","count"]).transpose()
-    completeness_df["Total Data"] = len(df)
+    completeness_df = completeness_df.isna().agg(["sum"]).transpose()
+    completeness_df["Complete Data"] = len(df) - completeness_df["sum"]
     completeness_df = round(completeness_df/len(df), 2)
+    print(completeness_df)
     completeness_df.reset_index(inplace=True)
-    completeness_df.rename(columns={"index":"Column","sum":"Missing Data","count":"Complete Data"},inplace=True)
+    completeness_df.rename(columns={"index":"Column","sum":"Missing Data"},inplace=True)
     completeness_df = completeness_df.melt(
         id_vars="Column", 
-        value_vars=["Missing Data", "Complete Data", "Total Data"],
+        value_vars=["Missing Data", "Complete Data"],
         var_name="Data Type",
         value_name="Proportion"
     )
@@ -22,9 +23,9 @@ def plot_completeness_barplot(df, column, df_name=None):
         x="Proportion",
         y="Column",
         hue="Data Type",
-        hue_order=["Total Data","Complete Data","Missing Data"],
-        palette = {"Complete Data": '#1f77b4', "Missing Data": '#d62728', "Total Data":"#2ca02c"},
-        edgecolor='black'
+        hue_order=["Complete Data","Missing Data"],
+        palette = {"Complete Data": '#1f77b4',"Missing Data": '#d62728'},
+        edgecolor="black"
     )
     plt.title(f"Barplot for {df_name} DataFrame Completeness" if df_name else "Barplot")
     plt.show()
@@ -42,7 +43,7 @@ def plot_uniqueness_barplot(df, column, df_name=None):
         x="Unique Values",
         y="Column",
         hue="Column",
-        edgecolor='black'
+        edgecolor="black"
     )
     plt.title(f"Barplot for {df_name} DataFrame Unique Values" if df_name else "Barplot")
     plt.show()
